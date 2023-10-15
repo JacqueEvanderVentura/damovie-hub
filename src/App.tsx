b9cb1307a69f90ce74ea271d7150d4b25b6052ad
JEVANDER <jacque.evander.ventura@gmail.com>
2023-10-15 19:38:35 -0400

    refactor(routing): new routing system implemented (same lib)

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,47 @@
 import { ReactNode } from "react";
-import LoginForm from "./components/LoginForm";
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate,
+} from "react-router-dom";
 import { useSelector } from "react-redux";
 
 import Topbar from "./components/Topbar";
-import Footer from "./components/Footer";
+import LoginForm from "./components/LoginForm";
 import Hub from "./components/Hub";
+import Footer from "./components/Footer";
 
 function App(): ReactNode {
 	const isLoggedIn = useSelector((state: any) => state.auth.isLoggedIn);
 
 	return (
-		<div className="flex flex-col bg-[#03067B] min-w-screen min-h-screen w-full h-full">
-			<Topbar />
-			<div className="flex-grow">{isLoggedIn ? <Hub /> : <LoginForm />}</div>
-			<Footer />
-		</div>
+		<Router>
+			<div className="flex flex-col bg-[#03067B] min-w-screen min-h-screen w-full h-full">
+				<Topbar />
+				<main className="flex justify-center min-h-screen flex-grow mx-5 md:mx-20 xl:mx-48">
+					<Routes>
+						<Route
+							path="/"
+							element={isLoggedIn ? <Navigate to="/hub" /> : <LoginForm />}
+						/>
+						<Route
+							path="/login"
+							element={isLoggedIn ? <Navigate to="/hub" /> : <LoginForm />}
+						/>
+						<Route
+							path="/hub/*"
+							element={isLoggedIn ? <Hub /> : <Navigate to="/login" />}
+						/>
+						<Route
+							path="*"
+							element={<Navigate to="/" />}
+						/>
+					</Routes>
+				</main>
+				<Footer />
+			</div>
+		</Router>
 	);
 }
 
